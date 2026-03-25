99bd4d42954aa1ec0b54f69400a3644fc399d13d
Grishma Doshi <114432849+grishmadosi@users.noreply.github.com>
2026-03-25 19:56:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (98.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return money(nums, n,0, dp);
-    }
-    public int money(int[] nums, int n, int idx,int[] dp){
-        if(idx == n-1) return nums[idx];
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
+        // int[] dp = new int[n];
+        // Arrays.fill(dp,-1);
+        // return money(nums, n,0, dp);
 
-        int notPick = money(nums, n , idx+1,dp);
-        int pick = money(nums,n,idx+2,dp) + nums[idx];
-        return dp[idx] = Math.max(pick,notPick);
+        //----Tabulation form
+        int[] dp = new int[n];
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
+    // public int money(int[] nums, int n, int idx,int[] dp){
+    //     if(idx == n-1) return nums[idx];
+    //     if(idx >= n) return 0;
+    //     if(dp[idx] != -1) return dp[idx];
+
+    //     int notPick = money(nums, n , idx+1,dp);
+    //     int pick = money(nums,n,idx+2,dp) + nums[idx];
+    //     return dp[idx] = Math.max(pick,notPick);
+    // }
 }
